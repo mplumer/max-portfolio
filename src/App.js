8bc9eaada16f6fa30b58625392735114443f5d83
Max Plumer <68719068+mplumer@users.noreply.github.com>
2021-01-18 12:24:14 -0500

    gh pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./components/Header";
+import About from "./components/Header/Navigation/About";
+import Portfolio from "./components/Header/Navigation/Portfolio";
+import Resume from "./components/Header/Navigation/Resume";
+import Contact from "./components/Header/Navigation/Contact";
+import SocialFollow from "./components/Footer";
 
 function App() {
+  const [currentPage, handlePageChange] = useState("Home");
+
+  const renderPage = (currentPage) => {
+    switch (currentPage) {
+      case "About":
+        return <About />;
+      case "Portfolio":
+        return <Portfolio />;
+      case "Resume":
+        return <Resume />;
+      case "Contact":
+        return <Contact />;
+      default:
+        return <About />;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main className="main">
+          <Header
+            currentPage={currentPage}
+            handlePageChange={handlePageChange}
+          />   
+        <div className="main-content">{renderPage(currentPage)}</div>
+      </main>
+      <footer className="footer">
+        <SocialFollow />
+      </footer>
+    </>
   );
 }
 
